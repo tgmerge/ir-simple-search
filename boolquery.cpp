bad54367376c20853d6f3dc936a246026fa82551
tgmerge <tgmgsp@gmail.com>
2014-06-15 21:38:29 +0800

    update boolquery

--- boolquery.cpp
+++ boolquery.cpp
@@ -4,51 +4,147 @@
 //
 
 #include "boolquery.h"
+#include <sstream>
+#include <stack>
 
 ResultSet BoolQuery::query(string queryStr) {
-	return doQuery(queryStr);
+	Expression infex = split(queryStr, ' ');
+
+	// debug
+	cout << "[query infex] ";
+	for (string t : infex) cout << t << ',';
+
+	Expression rpn = toRPN(infex);
+
+	// debug
+	cout << endl << "[query rpn] ";
+	for (string t : rpn) cout << t << ',';
+
+	ResultSet result = calcRPN(rpn);
+	return result;
 }
 
 
-ReverseTable BoolQuery::doQuery(string queryStr) {
-	/*
-	偷懒做法= =
-	a = None
-	b = None
-	switch(scan for next word)
-		case term:
-			a = getRT(term)
-			get op
-			b = getRT(剩下的所有部分)
-			result = doOp(a, b)
-		case '(':
-			a = getRT( doQuery(括号中间的部分) )
-			get op
-			b = getRT(剩下的所有部分)
-			result = doOp(a, b)
-		case NOT:
-			if '(':
-				result = doNot( doQuery(括号中间的部分) )
-			else:
-				a = getRT(term)
-				result = doNot(a)
-	*/
+ResultSet BoolQuery::calcRPN(Expression exp) {
+	stack<ReverseTable> tableStack;
+	ReverseTable opA, opB;
+
+	for (string token : exp) {
+		if (isOp(token)) {
+			opA = tableStack.top();
+			tableStack.pop();
+			if (token != "NOT") {
+				opB = tableStack.top();
+				tableStack.pop();
+			}
+			tableStack.push(doCalc(token, opA, opB));
+		}
+		else {
+			tableStack.push(getReverseTable(token));
+		}
+	}
+
+	return tableStack.top();
 }
 
 
 ReverseTable BoolQuery::getReverseTable(string term) {
-	// RETURN a reverse index table from vbcode.decoder(term)
-}
-ReverseTable BoolQuery::doAnd(const ReverseTable& table1, const ReverseTable& table2) {
-	
-}
-
-
-ReverseTable BoolQuery::doOr(const ReverseTable& table1, const ReverseTable& table2) {
-
+	// dummy table for testing
+	ReverseTable emptyTable;
+	return(emptyTable);
 }
 
 
-ReverseTable BoolQuery::doNot(const ReverseTable& table1) {
+ReverseTable BoolQuery::doCalc(string op, ReverseTable opA, ReverseTable opB) {
+	ReverseTable result;
+	int iA = 0, iB = 0;
+	if (op == "AND") {
+		while (iA < opA.size && iB < opB.size) {
+			if (opA[iA] == opB[iB])
+				result.push_back(opA[iA]);
+			else
+				if (opA[iA] < opB[iB]) iA++; else iB++;
+		}
+	}
+	else if (op == "OR") {
+		while (iA < opA.size || iB < opB.size) {
+			if (iA < opA.size && iB < opB.size && opA[iA] == opB[iB]) {
+				result.push_back(opA[iA]);
+				iA++;
+				iB++;
+			}
+			else if (iB >= opB.size || opA[iA] < opB[iB]) {
+				result.push_back(opA[iA]);
+				iA++;
+			}
+			else if (iA >= opA.size || opA[iA] > opB[iB]) {
+				result.push_back(opB[iB]);
+				iB++;
+			}
+		}
+	}
+	else if (op == "NOT") {
 
+	}
+}
+
+
+Expression BoolQuery::toRPN(Expression exp) {
+	stack<string> tmpStack;
+	Expression rpn;
+
+	for (string token : exp) {
+		if (token == "(") {
+			tmpStack.push(token);
+		}
+		else if (token == ")") {
+			while (tmpStack.top() != "(") {
+				rpn.push_back(tmpStack.top());
+				tmpStack.pop();
+			}
+			tmpStack.pop();
+		}
+		else if (isOp(token)) {
+			while (!tmpStack.empty() && priority(token) <= priority(tmpStack.top())) {
+				rpn.push_back(tmpStack.top());
+				tmpStack.pop();
+			}
+			tmpStack.push(token);
+		}
+		else { // token is a term
+			rpn.push_back(token);
+		}
+	}
+	while (!tmpStack.empty()) {
+		rpn.push_back(tmpStack.top());
+		tmpStack.pop();
+	}
+
+	return rpn;
+}
+
+
+bool BoolQuery::isOp(string token) {
+	return (token == "AND" || token == "OR" || token == "NOT");
+}
+
+
+int BoolQuery::priority(string op) {
+	if (op == "OR")  return 0;
+	if (op == "AND") return 1;
+	if (op == "NOT") return 2;
+	if (op == "(")   return -1;
+}
+
+
+vector<string> BoolQuery::split(const std::string &s, char delim) {
+	vector<string> elems;
+	stringstream ss(s);
+	string item;
+	while (std::getline(ss, item, delim)) {
+		if (!item.empty()) {
+			elems.push_back(item);
+		}
+	}
+	return elems;
 }
--- boolquery.h
+++ boolquery.h
@@ -12,22 +12,46 @@
 
 typedef vector<long> ReverseTable;
 typedef vector<long> ResultSet;
+typedef vector<string> Expression;
 
 class BoolQuery{
 
 public:
 
 	// accept a bool query string, returns vector of results(maybe docIDs)
+	// "this OR that" --> [ResultSet]
 	ResultSet query(string queryStr);
 
 private:
 
-	ReverseTable doQuery(string queryStr);
+	// 分割字符串
+	// usage: split("this is", ' ');
+	Expression split(const std::string &s, char delim);
 
+	// 检查token是否是运算符
+	bool isOp(string token);
+
+	// 返回token的优先级
+	int priority(string op);
+
+	// 中缀表达式->后缀表达式
+	// e.g. (Expression)a AND b --> (Expression)a b AND
+	Expression toRPN(Expression exp);
+
+	// 计算后缀表达式
+	// TOOD 测试
+	// e.g. (Expression)a b AND --> (ResultSet)1 2 4 6
+	ResultSet calcRPN(Expression exp);
+	
+	// 从decoder获取词项term的倒排索引
+	// TODO
+	// e.g. "doge" --> (ResultSet)1 2 5 8
 	ReverseTable getReverseTable(string term);
-	ReverseTable doAnd(const ReverseTable& table1, const ReverseTable& table2);
-	ReverseTable doOr(const ReverseTable& table1, const ReverseTable& table2);
-	ReverseTable doNot(const ReverseTable& table1);
 
+	// 进行一次and, or, not计算
+	// TOOD NOT
+	// e.g. "AND", (table), (table) --> (table)1 5 7
+	ReverseTable doCalc(string op, ReverseTable opA, ReverseTable opB);
 };
+
 #endif
